eee94eeef5fcbaee3d819cfaafce213a03f53837
Md_Rahmat_Ullah_Khan <mdrahmatullahkhanshakib@gmail.com>
2026-01-09 23:11:46 +0600

    commit5

--- lib/ui/screens/forgot_password_verify_otp_screen.dart
+++ lib/ui/screens/forgot_password_verify_otp_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager/ui/screens/forgot_password_verify_otp_screen.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:task_manager/ui/widgets/screen_background.dart';
 
-class ForgotPasswordEmailVerify extends StatelessWidget {
-  const ForgotPasswordEmailVerify({super.key});
+class ForgotPasswordVerifyOtpScreen extends StatefulWidget {
+  const ForgotPasswordVerifyOtpScreen({super.key});
 
+  @override
+  State<ForgotPasswordVerifyOtpScreen> createState() => _ForgotPasswordVerifyOtpScreenState();
+}
+
+class _ForgotPasswordVerifyOtpScreenState extends State<ForgotPasswordVerifyOtpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,30 +21,43 @@ class ForgotPasswordEmailVerify extends StatelessWidget {
           children: [
             const SizedBox(height: 200),
             Text(
-              "Your Email Address",
+              "PIN Verification",
               style: Theme.of(context).textTheme.titleLarge,
             ),
 
             const SizedBox(height: 10),
             Text(
-              "A 6 digits code will be sent to your email address",
+              "A 6 digits code  sent to your email address",
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
                 color: Colors.grey,
               ),
             ),
 
             const SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Email',
+
+            PinCodeTextField(
+              length: 6,
+              obscureText: false,
+              animationType: AnimationType.fade,
+              keyboardType: TextInputType.number,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(7),
+                fieldHeight: 50,
+                fieldWidth: 40,
+                activeFillColor: Colors.white,
+                inactiveColor: Colors.grey.shade300,
+                selectedColor: Colors.green,
               ),
+              animationDuration: Duration(milliseconds: 300),
+              backgroundColor: Colors.white,
+              //enableActiveFill: true,
+              appContext: context,
             ),
             
             const SizedBox(height: 16),
             FilledButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => ForgotPasswordVerifyOtpScreen()));
-              }, 
+              onPressed: (){}, 
               child: Icon(Icons.arrow_circle_right_outlined),
             ),
 
