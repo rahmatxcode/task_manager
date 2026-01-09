8f4a176f2b9f688bd15d89790d3b9136e06402ab
Md_Rahmat_Ullah_Khan <mdrahmatullahkhanshakib@gmail.com>
2026-01-09 09:23:07 +0600

    second commit

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:task_manager/ui/screens/login_page.dart';
 import 'package:task_manager/ui/utils/asset_path.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -26,22 +27,14 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children:[
-          SvgPicture.asset(
-            AssetPaths.backgroundSVG,
-            width: double.maxFinite,
-            height: double.maxFinite,
-            fit: BoxFit.cover,
-          ),
-          Center(
-            child: SvgPicture.asset(
+      body: ScreenBackground(
+        child: Center(
+          child: SvgPicture.asset(
             AssetPaths.logoSVG,
             width: 100,
             height: 100,
           ),
-          ),
-        ],
+        ),
       ),
     );
   }
