c69ac6cf5f06d214e776c5a0351b0b663d2361eb
Md_Rahmat_Ullah_Khan <mdrahmatullahkhanshakib@gmail.com>
2026-01-20 23:48:38 +0600

    commit8

--- lib/ui/screens/new_task_screen.dart
+++ lib/ui/screens/new_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/widgets/task_card.dart';
 import 'package:task_manager/ui/widgets/task_count_by_status.dart';
 import 'package:task_manager/ui/widgets/tm_app_bar.dart';
 
@@ -13,22 +14,43 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: TMAppBar(),
       body: Column(
         children: [
           SizedBox(height: 15.0),
 
-          SizedBox(
-            height: 90.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 4,
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: SizedBox(
+              height: 90.0,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index){
+                  return TaskCountByStatus(
+                    title: 'Cancelled',
+                    count: index+5,
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return SizedBox(width: 4.0);
+                }
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: ListView.separated(
+              itemCount: 10,
               itemBuilder: (context, index){
-                return TaskCountByStatus(
-                  title: 'Cancelled',
-                  count: index+5,
+                return TaskCard();
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(
+                  height: 4.0,
                 );
-              }
+              },
             ),
           ),
         ],
---
+++ lib/ui/widgets/task_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Card(
+        color: Colors.white,
+        child: ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          title: Text('This is task title'),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('This is task description'),
+              Text('Date: 20-01-2026'),
+              Row(
+                children: [
+                  Chip(
+                    label: Text('New'),
+                    backgroundColor: Colors.blue,
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 18.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit_note_rounded, color: Colors.green,),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete, color: Colors.red,),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
